d145defdc2d7ea8f4161052e26f5acc3b5a050bd
Alex <timejumper1984@gmail.com>
2021-05-10 05:28:37 +0800

    update 0013 Roman to Integer

--- 20210509_0013_Roman_to_Integer.py
+++ 20210509_0013_Roman_to_Integer.py
@@ -4,8 +4,33 @@
 # @File : 20210509_0013_Roman_to_Integer.py
 
 
-# def romanToInt(s: str) -> int:
-
+def romanToInt(s: str) -> int:
+    trans = dict()
+    trans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # print(trans)
+    l = len(s)
+    result = 0
+    # prenum = trans[s[0]]
+    for i in range(l):
+        if i == l-1:
+            # print(result)
+            if result >= trans[s[i]]:
+                result = trans[s[i]] + result
+            else:
+                result = trans[s[i]] - result
+            return result
+        else:
+            # print(i,l)
+            if trans[s[i]] >= trans[s[i+1]]:
+                result = trans[s[i]] + result
+                # print(i,result,trans[s[i]],trans[s[i+1]])
+            else:
+                result = trans[s[i]] - result
+                # print(i,result,trans[s[i]],trans[s[i+1]])
 
 if __name__ == '__main__':
+    s = str(input())
+
+    print(romanToInt(s))
+
     print('program done')
